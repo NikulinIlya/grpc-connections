c3acfdc53a1d90c53db3ec24b8b704f04f235812
Илья Никулин <i.nikulin@sfbtech.org>
2019-10-02 19:12:26 +0300

    feat(cmd): make server

--- cmd/gRPC/server/main.go
+++ cmd/gRPC/server/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	pb "projects/grpc-connections/internal/gRPC/connection"
+	"projects/grpc-connections/internal/gRPC/impl"
+)
+
+const (
+	portOne  = 7000
+	portTwo  = 7001
+	portTree = 7002
+)
+
+func runServe(port uint) {
+	netListenerOne := getNetListener(port)
+
+	gRPCServer := grpc.NewServer()
+
+	connectionServiceImpl := impl.NewConnectionServiceGrpcImpl()
+	pb.RegisterConnectionServiceServer(gRPCServer, connectionServiceImpl)
+
+	// start the server
+	log.Printf("Start Server One: %d", port)
+	if err := gRPCServer.Serve(netListenerOne); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
 
 func main() {
-	fmt.Println("gRPC In Action!")
+	go runServe(portOne)
+	go runServe(portTwo)
+	runServe(portTree)
+}
+
+func getNetListener(port uint) net.Listener {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+		panic(fmt.Sprintf("failed to listen: %v", err))
+	}
+
+	return lis
 }
