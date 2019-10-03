9ed1176c50fcaa6477d9feffafd44fa9d30d021a
Илья Никулин <i.nikulin@sfbtech.org>
2019-10-03 16:34:08 +0300

    feat(ConnectionServiceGrpcImpl): comment println received data

--- internal/grpc/impl/ConnectionServiceGrpcImpl.go
+++ internal/grpc/impl/ConnectionServiceGrpcImpl.go
@@ -2,7 +2,6 @@ package impl
 
 import (
 	"context"
-	"log"
 	"projects/grpc-connections/internal/grpc/connection"
 )
 
@@ -15,7 +14,7 @@ func NewConnectionServiceGrpcImpl() *ConnectionServiceGrpcImpl {
 }
 
 func (serviceImpl *ConnectionServiceGrpcImpl) SendClick(ctx context.Context, in *connection.ClickData) (*connection.ClickResponse, error) {
-	log.Println("Received data:", in.Id, in.Connection.Host, in.Connection.Port, "Click Id:", in.Click.Id)
+	//log.Println("Received data:", in.Id, in.Connection.Host, in.Connection.Port, "Click Id:", in.Click.Id)
 
 	return &connection.ClickResponse{
 		Click:      in.Click,
